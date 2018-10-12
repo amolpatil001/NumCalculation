dcdb90b711e5517434aabc888d63e865bc1128e9
Amol Patil <amolrpatil001@yahoo.com>
2018-10-12 16:50:15 +0530

    Default controller

---
+++ NumCalculation/Controllers/CalculatorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NumCalculation.Models;
+
+namespace NumCalculation.Controllers
+{
+    public class CalculatorController : Controller
+    {
+		// GET: Calculator
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+
+		public ActionResult Result(ModelAddition modelBinders)
+		{
+			ViewBag.result = modelBinders.firstValue + modelBinders.secondValue;
+
+			return View("~/Views/Calculator/Index.cshtml");
+
+		}
+	}
+}
---
+++ NumCalculation/Models/ModelAddition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NumCalculation.Models
+{
+	public class ModelAddition
+	{
+		[Required]
+		public double firstValue { get; set; }
+
+		[Required]
+		public double secondValue { get; set; }
+		public string total { get; set; }
+	}
+}
